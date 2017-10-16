d88033ecdbb4700a28318bc07f2a70edb548c0c4
Olarewaju Opeyemi <olarewajuakeemope@icloud.com>
2017-10-16 20:25:33 +0100

    chore(relay-fix):Fix relay component rendering

--- js/common/ListContainer.js
+++ js/common/ListContainer.js
@@ -47,16 +47,41 @@ type State = {
 
 const EMPTY_CELL_HEIGHT = Dimensions.get('window').height > 600 ? 200 : 150;
 
-class RelayLoading extends Component {
+var Relay = require('react-relay/classic');
+var RelayRenderer = require('react-relay/lib/RelayRenderer.js');
+
+class MainRoute extends Relay.Route {}
+MainRoute.queries = { viewer: () => Relay.QL`query { viewer }` };
+MainRoute.routeName = 'MainRoute';
+
+class RelayLoading extends React.Component {
   render() {
+    const child = React.Children.only(this.props.children);
+    if (!child.type.getFragmentNames) {
+      return child;
+    }
     return (
-      <View style={{ height: 400 }}>
-          <Text>child.props.renderHeader and child.props.renderHeader</Text>
+      <RelayRenderer
+        Container={child.type}
+        queryConfig={new MainRoute()}
+        environment={Relay.Store}
+        render={({props}) => this.renderChild(child, props)}
+      />
+    );
+  }
+
+  renderChild(child, props) {
+    if (!props) {
+      return (
+        <View style={{height: 400}}>
+          {child.props.renderHeader && child.props.renderHeader()}
           <View style={{flex: 1, backgroundColor: 'white', alignItems: 'center', justifyContent: 'center'}}>
             <ActivityIndicator />
           </View>
         </View>
-    );
+      );
+    }
+    return React.cloneElement(child, {...this.props, ...props});
   }
 }
 
